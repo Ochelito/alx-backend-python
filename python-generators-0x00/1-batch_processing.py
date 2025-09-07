85bd437d0a635476384662d81baa0039d9d8ec3f
Idoko Augustine <linkwithaugustine@gmail.com>
2025-09-07 18:07:13 +0100

    added return statement for batch processing functions

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -35,7 +35,7 @@ def stream_users_in_batches(batch_size):
             cursor.close()
         if connection:
             connection.close()
-
+    return  # Explicitly end the generator
 
 def batch_processing(batch_size):
     """
@@ -45,3 +45,4 @@ def batch_processing(batch_size):
         for user in batch:
             if user["age"] > 25:
                 print(user)
+    return  # Explicitly end the generator
